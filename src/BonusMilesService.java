59143e1f06ae393b1ecf8a008a332330ee391561
Kostya Lachin <prikolist04@mail.ru>
2025-01-31 19:51:24 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+
+    public int calculate(int miles) {
+            int result;
+            if (miles > 1000) {
+            result = miles / 20;
+            } else {
+             result = 0;
+            }
+        return result;
+    }
+}
